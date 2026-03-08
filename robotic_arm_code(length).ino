2af8e26ed518d09f8477a735607f19d4c0e8a9d8
teiii <79211121+somehowtei@users.noreply.github.com>
2026-03-08 20:07:31 +0200

    Update robotic_arm_code(length).ino

--- robotic_arm_code(length).ino
+++ robotic_arm_code(length).ino
@@ -1,21 +1,21 @@
 #include <Servo.h>
-int joystickx_pin=A7;
-int joysticky_pin=A6;
-float joystickx_val;
+int joystickx_length_pin=A7;
+int joysticky_length_pin=A6;
+float joysticky_length_val;
 Servo servo_length;
 int servo_length_pin=11;
 float servo_length_val;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(joystickx_pin, INPUT);
+  pinMode(joysticky_length_pin, INPUT);
   servo_length.attach(servo_length_pin);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  joystickx_val=analogRead(joystickx_pin);
-  servo_length_val=(180./1023)*joystickx_val;
+  joysticky_val=analogRead(joysticky_length_pin);
+  servo_length_val=(180./1023)*joysticky_length_val;
   Serial.println(servo_length_val);
   servo_length.write(servo_length_val);
   delay(10);
