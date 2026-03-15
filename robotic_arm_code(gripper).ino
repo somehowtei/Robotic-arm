50bb500fbf18e5534a22744eb78c20fab5cdd3e0
teiii <79211121+somehowtei@users.noreply.github.com>
2026-03-15 19:37:34 +0200

    Adjust gripper threshold from 100 to 90

--- robotic_arm_code(gripper).ino
+++ robotic_arm_code(gripper).ino
@@ -1,9 +1,11 @@
 #include <Servo.h>
+
 int potentiometer_pin=A0;
 int potentiometer_val;
 Servo servo_gripper;
 int servo_gripper_pin=12;
 int servo_gripper_val;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -16,7 +18,7 @@ void loop() {
   potentiometer_val=analogRead(potentiometer_pin);
   servo_gripper_val=(180./1023)*potentiometer_val;
   Serial.println(servo_gripper_val);
-  if(servo_gripper_val<100)
+  if(servo_gripper_val<90)
   {
     servo_gripper.write(servo_gripper_val);
     
