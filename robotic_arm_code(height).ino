8bf2fd7a1b8287c1c3e882ee116397f4856c5e1b
teiii <79211121+somehowtei@users.noreply.github.com>
2026-03-16 10:51:28 +0200

    Update robotic_arm_code(height).ino

--- robotic_arm_code(height).ino
+++ robotic_arm_code(height).ino
@@ -1,22 +1,25 @@
-#include <Servo.h>
-int joystickx_height_pin=A4;
-int joysticky_height_pin=A5;
-float joystickx_height_val;
-Servo servo_height;
-int servo_height_pin=10;
-float servo_height_val;
+#include <VarSpeedServo.h>
+
+int joystickx_pin=A7;
+int joystickx_val;
+VarSpeedServo servo_height;
+int servo_height_pin=9;
+int servo_height_val;
+int speed=40;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(joystickx_height_pin, INPUT);
+
+  pinMode(joystickx_pin, INPUT);
   servo_height.attach(servo_height_pin);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  joystickx_height_val=analogRead(joystickx_height_pin);
-  servo_height_val=(180./1023)*joystickx_height_val;
+  joystickx_val=analogRead(joystickx_pin);
+  servo_height_val=(180./1023)*joystickx_val;
   Serial.println(servo_height_val);
-  servo_height.write(servo_height_val);
+  servo_height.write(servo_height_val, speed, false);
   delay(10);
 }
