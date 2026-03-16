2bd25d638194fe8f62d077e3dca7e329d9e5e112
teiii <79211121+somehowtei@users.noreply.github.com>
2026-03-16 10:57:51 +0200

    Refactor robotic arm code to use VarSpeedServo

--- robotic_arm_code(length).ino
+++ robotic_arm_code(length).ino
@@ -1,21 +1,23 @@
-#include <Servo.h>
-int joystickx_length_pin=A7;
-int joysticky_length_pin=A6;
-float joysticky_length_val;
-Servo servo_length;
+#include <VarSpeedServo.h>
+int joysticky_pin=A6;
+float joysticky_val;
+VarSpeedServo servo_length;
 int servo_length_pin=11;
 float servo_length_val;
+int speed=40;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(joysticky_length_pin, INPUT);
+
+  pinMode(joysticky_pin, INPUT);
   servo_length.attach(servo_length_pin);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  joysticky_length_val=analogRead(joysticky_length_pin);
-  servo_length_val=(180./1023)*joysticky_length_val;
+  joysticky_val=analogRead(joysticky_pin);
+  servo_length_val=(180./1023)*joysticky_val;
   Serial.println(servo_length_val);
   servo_length.write(servo_length_val);
   delay(10);
